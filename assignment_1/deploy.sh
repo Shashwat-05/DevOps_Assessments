d0d45ae66146b74f0b7f9f475aeebf7039d631c0
Shashwat-05 <shashwatpathak100@gmail.com>
2022-09-01 14:18:03 +0530

    updated deploy scripts

--- assignment_1/deploy.sh
+++ assignment_1/deploy.sh
@@ -4,4 +4,4 @@ sudo docker-compose run Budgy rake db:reset
 
 sudo docker-compose run Budgy rake db:migrate
 
-sudo docker-compose -f docker-compose.yml -e IMGVERSION=$1
+sudo docker-compose -f docker-compose.yml -e IMGVERSION=$1 up
