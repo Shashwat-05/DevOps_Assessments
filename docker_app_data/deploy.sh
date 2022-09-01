c72a985e13abc03deb92ec0ad02d764a205a9ee6
Shashwat-05 <shashwatpathak100@gmail.com>
2022-09-01 16:40:24 +0530

    env file updates

--- docker_app_data/deploy.sh
+++ docker_app_data/deploy.sh
@@ -4,4 +4,4 @@ sudo docker-compose run Budgy rake db:reset
 
 sudo docker-compose run Budgy rake db:migrate
 
-sudo docker-compose -f docker-compose.yml up -d
+sudo docker-compose -f docker-compose.yml --env-file .env up -d
